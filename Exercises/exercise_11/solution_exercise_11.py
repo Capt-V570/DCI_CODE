c51d3258fb68ad3d967f0c36f12482690c1ad8c7
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-26 12:10:25 +0200

    Modified Solution to Exercise 11

--- Exercises/exercise_11/solution_exercise_11.py
+++ Exercises/exercise_11/solution_exercise_11.py
@@ -2,13 +2,13 @@
 #########  replace() in python  ##########
 ##########################################
 
-print("""**********************************************
-**** replace('dog' with 'cat') in Python *****
-**********************************************""")
+print("""************************************
+**** replace('word') in Python *****
+************************************""")
 
 sentence = """A dogmatic dog buys dogecoin to become rich and
 buy hotdogs every day."""
 
-new_sentence = sentence.replace("dog", "cat")
+new_sentence = sentence.replace(" dog ", " cat ")
 
 print(f"{sentence}\nOutput: {new_sentence}")
