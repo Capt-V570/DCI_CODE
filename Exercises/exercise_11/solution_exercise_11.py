91cb027a96326b2bdafaee0f089da146c93909ed
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-26 13:49:39 +0200

    Added Version 2 to Solution Exercise 11

--- Exercises/exercise_11/solution_exercise_11.py
+++ Exercises/exercise_11/solution_exercise_11.py
@@ -9,6 +9,12 @@ print("""************************************
 sentence = """A dogmatic dog buys dogecoin to become rich and
 buy hotdogs every day."""
 
+# VERSION 1 - (SOLUTION WITH TWO LINES OF CODE)
 new_sentence = sentence.replace(" dog ", " cat ")
 
 print(f"{sentence}\nOutput: {new_sentence}")
+
+"""
+# VERSION 2 - (SOLUTION WITH JUST ONE LINE OF CODE) 
+print(f"{sentence}\nOutput: {sentence.replace(' dog ', ' cat ')}")
+"""
