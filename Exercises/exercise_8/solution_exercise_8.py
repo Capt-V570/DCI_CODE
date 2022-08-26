34f38a934b39d9964b1df1b760e0e23353b0ebaf
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-26 14:12:45 +0200

    Adjusting Exercise 8

--- Exercises/exercise_8/solution_exercise_8.py
+++ Exercises/exercise_8/solution_exercise_8.py
@@ -6,29 +6,6 @@ print("""***************************
 ******** Find NEMO ********
 ***************************""")
 
-
-#word_to_search = "Nemo"
-
-# def find_text():
-#     user_input = ""
-#     word_to_search = "Nemo"
-#     start_point = user_input.index("Nemo")
-#     return user_input[start_point]
-
-# word_to_search = "Nemo"
-# nemo_found = ""
-
-# while nemo_found:
-#     user_input = str(input("Enter a Text with word 'Nemo', here: "))
-#     if user_input == "End":
-#         break
-#     elif user_input != word_to_search:
-#         print("I can't find Nemo :(")
-#     elif user_input == word_to_search:
-#         print("I found Nemo at", find_text() + "!")
-#     else:
-#         print("whatever")
-
 def finding_Nemo():
     user_input = str(input("Enter a Text with word 'Nemo' in it, here: "))
     index = user_input.find("Nemo")
