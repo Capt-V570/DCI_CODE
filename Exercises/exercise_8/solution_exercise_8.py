09d131a0b114917fe58f511633a9e332649b5a25
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-26 14:11:12 +0200

    Attempting Adjusting Exercise 8 with a While Loop

--- Exercises/exercise_8/solution_exercise_8.py
+++ Exercises/exercise_8/solution_exercise_8.py
@@ -30,17 +30,22 @@ print("""***************************
 #         print("whatever")
 
 def finding_Nemo():
-    while finding_Nemo():
-        user_input = str(input("Enter a Text with word 'Nemo' in it, here: "))
-        index = user_input.find("Nemo")
-        if user_input == "End":
-            break
-        elif index == -1:
-            print("I can't find Nemo :(")
-        else:
-            print(f"I found Nemo at {index}!")
+    user_input = str(input("Enter a Text with word 'Nemo' in it, here: "))
+    index = user_input.find("Nemo")
+    if index == -1:
+        print("I can't find Nemo :(")
+    else:
+        print(f"I found Nemo at {index}!")
 
 finding_Nemo()
 print()
 
-# STILL TO MODIFIY
+# creating While Loop - TO CHECK
+# nemoing = ""
+# while nemoing:
+#     finding_Nemo()
+#     if user_input == "End":
+#         break
+#     else:
+#         print()
+
