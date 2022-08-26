a08e12ec7418cb82c53310a4c0332a46519a1b2a
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-26 12:07:05 +0200

    Commit Solution Exercise 11

---
+++ Exercises/exercise_11/solution_exercise_11.py
@@ -0,0 +1,14 @@
+##########################################
+#########  replace() in python  ##########
+##########################################
+
+print("""**********************************************
+**** replace('dog' with 'cat') in Python *****
+**********************************************""")
+
+sentence = """A dogmatic dog buys dogecoin to become rich and
+buy hotdogs every day."""
+
+new_sentence = sentence.replace("dog", "cat")
+
+print(f"{sentence}\nOutput: {new_sentence}")
