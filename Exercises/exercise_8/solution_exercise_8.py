6cb8c872b460a71cd12b7feba6768fb2a3f71897
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-26 10:30:20 +0200

    Initial Commit Ex 8

--- Exercises/exercise_8/solution_exercise_8.py
+++ Exercises/exercise_8/solution_exercise_8.py
@@ -2,3 +2,26 @@
 ########## FIND Function in Python ##########
 #############################################
 
+print("""***************************
+******** Find NEMO ********
+***************************""")
+
+user_input = str(input("Enter a Text with word 'Nemo' in it, here: "))
+word_to_search = "Nemo"
+
+def find_text(user_input, word_to_search):
+    start = user_input.index("Nemo")
+    return user_input[start]
+
+nemo_found = ""
+
+while nemo_found:
+    user_input = str(input("Enter a Text with word 'Nemo', here: "))
+    if user_input == "End":
+        break
+    elif user_input != word_to_search:
+        print("I can't find Nemo :(")
+    elif user_input == word_to_search:
+        print("I found Nemo at", find_text() + "!")
+    else:
+        print("whatever")
