f2bc9f472e9c20ba1f9882d5e4fbbc8a583fecfd
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-09 15:08:30 +0200

    Add regex_exercise_2.py to Regular_Expression Folder

--- Exercises/Regular_Expressions/Reg_Ex_Text.py
+++ Exercises/Regular_Expressions/Reg_Ex_Text.py
@@ -5,7 +5,6 @@
 import re           # importing library to work with RegEx
 
 # target text
-
 text_to_be_searched = """Hey Mr. Bezos,
                         I hope its okay I message you in this unsecure email program. Sry about that!
                         Here the list of extremely confidential clients and close coworkers of you who actually visited Epsteins island. 
@@ -24,8 +23,10 @@ text_to_be_searched = """Hey Mr. Bezos,
                         the soon to be fired secretary Tanisha
                         """
 
-pattern = "\w+@\w+.\W+"
+pattern = "\w+@\w+.\w+"                             # [\w] search for all any letter, digit or underscore. Equivalent to [a-zA-Z0-9_]
+                                                    # [+] search for one or more repetions of any letter, digit or underscore
+                                                    # [.\w] after a dot[.] - it search for all any letter, digit or underscore, Equivalent to [a-zA-Z0-9_] 
 
-x = re.findall(pattern, text_to_be_searched)
+x = re.findall(pattern, text_to_be_searched)        # creating variable with method to find all items matching the pattern in the target text
 
-print(x)
+print(x)                                            # print list of emails 
