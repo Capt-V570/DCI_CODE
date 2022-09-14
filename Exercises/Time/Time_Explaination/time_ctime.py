ea1dc038ca33d66ca3d1661625cc923281f1a22a
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-14 13:36:39 +0200

    Adjust Time Folder ctime.py

--- Exercises/Time/Time_Explaination/time_ctime.py
+++ Exercises/Time/Time_Explaination/time_ctime.py
@@ -1,6 +1,6 @@
 import time
 
-x = time.time()   # current time in seconds since unix epoch
+x = time.time()   # current time in seconds since start of UNIX Epoch
 y = time.ctime(2147483647) # x converted to human readable time format
 
 # print(x)       
