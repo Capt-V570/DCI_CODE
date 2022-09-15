b5c38cd36d65426f9180f2126bfe28405353da31
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-15 18:06:54 +0200

    Start DateTime_Exercises Task1.py

--- Exercises/Time/DateTime_Exercises/DateTime_Task1.py
+++ Exercises/Time/DateTime_Exercises/DateTime_Task1.py
@@ -2,4 +2,14 @@
 ####### DateTime - Task 1 ##########
 ####################################
 
-from datetime import datetime
+from datetime import datetime, date
+import time
+
+current_datetime = datetime.now()
+
+print(dir(current_datetime))
+
+# Task 1
+print("###### Task 1 ######")
+
+
