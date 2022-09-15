19f54cf96acd54b1fa9dbc636708884ac3fc50e4
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-15 14:15:43 +0200

    Modify name to DateTime_Explanation_basic.py

--- Exercises/Time/DateTime_Explaination/DateTime_Explaination_basics.py
+++ Exercises/Time/DateTime_Explaination/DateTime_Explaination_basics.py
@@ -14,12 +14,17 @@ date_as_string = "2022-09-15"
 
 print(date.fromisoformat(date_as_string))
 
+
+# Now we gonna create some datetime objects
+
 # How to create a datetime object with time:
-date_1 = datetime(2022, 1, 26, 10, 30, 59)
+date_1 = datetime(2022, 1, 26, 10, 30, 59)                                      # simple version
+#date_1 = datetime(year=2022, month=1, day=26, hour=10, month=30, second=59)     # verbose version
 
 print(date_1)
 
 # How to create a datetime object with just the date:
-date_2 = date(2022, 1, 26)
+date_2 = date(2022, 1, 26)                      # simple version                          
+#date_2 = date(year=2022, month=1, day=26)       # verbose version
 
 print(date_2)
