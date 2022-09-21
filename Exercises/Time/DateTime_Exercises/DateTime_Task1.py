216a8e2c3e66c2a82efda32680d09c0b145a5226
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-21 15:22:57 +0200

    Add Task2 to DateTime exercise1

--- Exercises/Time/DateTime_Exercises/DateTime_Task1.py
+++ Exercises/Time/DateTime_Exercises/DateTime_Task1.py
@@ -2,19 +2,13 @@
 ####### DateTime - Task 1 ##########
 ####################################
 
-from datetime import datetime, date
+from datetime import datetime
 import time
 
 current_datetime = datetime.now()
 
-'''
+
 print(dir(current_datetime))      # print the dir means The dir() function returns all properties and methods of the specified object, without the values. This function will return all the properties and methods, even built-in properties which are default for all object.
-'''
-
-# Task 1
-print("###### Task 1 ######")
-
-print(current_datetime.year)
 
 
 # Task 1
@@ -22,3 +16,15 @@ print("###### Task 1 ######")
 
 print(current_datetime.year)
 
+
+# Task 2
+print("###### Task 2 ######")
+
+some_date = datetime(2021, 7, 14)
+
+current_date = datetime.isoweekday(some_date)
+
+print(current_date)
+
+# Task 3
+print("###### Task 3 ######")
