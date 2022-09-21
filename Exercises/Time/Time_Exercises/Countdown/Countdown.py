938ed614d5c2d7cc5827f5204afbed1fa18e7796
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-21 09:55:09 +0200

    Modify Countdown.py

--- Exercises/Time/Time_Exercises/Countdown/Countdown.py
+++ Exercises/Time/Time_Exercises/Countdown/Countdown.py
@@ -2,20 +2,20 @@
 import time         # import the time module
   
 # define the countdown func.
-def countdown(t):
+def countdown(time_set):
     
-    while t:
-        mins, secs = divmod(t, 60)
+    while time_set:
+        mins, secs = divmod(time_set, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
-        t -= 1
+        time_set -= 1
       
     print('Time is Up!!!')
   
   
 # input time in seconds
-t = input("Enter the time in seconds: ")
+time_set = input("Enter the time in seconds: ")
   
 # function call
-countdown(int(t))
+countdown(int(time_set))
