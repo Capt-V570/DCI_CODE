3fb21a0892c6a5e402dbcafd351ff1d62c173846
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-22 11:11:14 +0200

    Add Task 1 and 2 compplition to Exercise 2 in DateTime Folder

--- Exercises/Time/DateTime_Exercises/DateTime_evercise_2.py
+++ Exercises/Time/DateTime_Exercises/DateTime_evercise_2.py
@@ -1,5 +1,28 @@
-####################################
-####### DateTime - Task 2 ##########
-####################################
+######################################
+####### DateTime - Basics 2 ##########
+######################################
 
 from datetime import datetime
+
+current_datetime = datetime.now()
+
+print("\nPREFACE\nCurrent datetime is:", current_datetime)
+
+# Task 1
+print("\n###### Task 1 ######\n")
+
+from datetime import timedelta          # importing timedelta module
+
+two_weeks_prior = timedelta(days = 15)       # setting timedelta variable to 15 days
+
+print(current_datetime - two_weeks_prior)    # subtracting the 15 days timedelta from current datetime and printing it
+
+# Task 2
+print("\n###### Task 2 ######\n")
+
+week_ahead = timedelta(days = 7)        # setting timedelta to 7 days
+
+print(current_datetime + week_ahead)    # adding the 7 days timedelta to current datetime and printing it
+
+
+
