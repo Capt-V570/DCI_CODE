2a43f11f43f96188edd91fa70bf0c5cddaa99c25
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-22 11:58:40 +0200

    Start Exercise 3 in DateTime Folder

---
+++ Exercises/Time/DateTime_Exercises/DateTime_exercise_3.py
@@ -0,0 +1,13 @@
+######################################
+####### DateTime - Basics 3 ##########
+######################################
+
+from datetime import datetime
+from time import strftime
+
+current_datetime = datetime.now()
+
+print("\nPRECONDITION\nCurrent datetime is:", current_datetime)
+
+# Task 1
+print("\n###### Task 1 ######\n")
