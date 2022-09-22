4b7fa334042d4e26c6107107f1f57e266e1bdb82
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-22 11:42:42 +0200

    Add Task 3 final to Exercise 2 in DateTime Folder

--- Exercises/Time/DateTime_Exercises/DateTime_evercise_2.py
+++ Exercises/Time/DateTime_Exercises/DateTime_evercise_2.py
@@ -3,10 +3,11 @@
 ######################################
 
 from datetime import datetime
+from time import strftime
 
 current_datetime = datetime.now()
 
-print("\nPREFACE\nCurrent datetime is:", current_datetime)
+print("\nPRECONDITION\nCurrent datetime is:", current_datetime)
 
 # Task 1
 print("\n###### Task 1 ######\n")
@@ -23,6 +24,20 @@ print("\n###### Task 2 ######\n")
 week_ahead = timedelta(days = 7)        # setting timedelta to 7 days
 
 print(current_datetime + week_ahead)    # adding the 7 days timedelta to current datetime and printing it
+
+# Task 3
+print("\n###### Task 3 ######\n")
+
+start_date = datetime(year=2020, month=1, day=1)
+
+payment_due = timedelta(days = 390)     # setting timedelta to 1 year and 25 days
+
+rent_due = start_date + payment_due
+
+format_rent_due = rent_due.strftime("%d %B, %Y")
+
+print("Hello Friedrich, your rent of 300 € is due on", format_rent_due)
+
 
 
 
