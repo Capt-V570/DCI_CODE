edebf2f296b6745feaf30ffd7cdde14b217fcec8
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-23 11:59:09 +0200

    Modify app.py to Functions_Theory Folder

---
+++ Exercises/Functions/Functions_Theory/app.py
@@ -0,0 +1,5 @@
+# prone to errors !!! logical data entry errors
+print(full_name('last name', 'first name'))
+# very hard to make mistakes!
+print(full_name(first_name='first name', last_name='last name'))
+print(full_name(last_name='last name', first_name='first name'))
