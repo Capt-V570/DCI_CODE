b4df082cdc0a71c899e49a4c98740716bcce0656
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-23 15:22:59 +0200

    Add additional modified Files in Functions

--- Exercises/Functions/Functions_Theory/app.py
+++ Exercises/Functions/Functions_Theory/app.py
@@ -1,3 +1,15 @@
+
+
+from sys import last_traceback
+
+
+def greeting(first_name, last_name, salutation ="Dear Sir/Madam"):
+    return f"{salutation} {first_name}, {last_name,}, you are invited to a gala evening!"
+
+full_name = "Fausto Ferrara"
+first_name = "Fausto"
+last_name = "Ferrara"
+
 # prone to errors !!! logical data entry errors
 print(full_name('last name', 'first name'))
 # very hard to make mistakes!
