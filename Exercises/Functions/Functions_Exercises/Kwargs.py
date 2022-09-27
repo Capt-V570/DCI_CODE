0f69efe9caa907171ffbdd89dcccafbf4188a54a
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-27 09:38:40 +0200

    Modified Kwargs.py Explaination

--- Exercises/Functions/Functions_Exercises/Kwargs.py
+++ Exercises/Functions/Functions_Exercises/Kwargs.py
@@ -2,19 +2,40 @@
 ####### KWARGS (Keywords arguments) ########
 ############################################
 
-
-# *args! (*kwargs)
+'''
+we are going to explain KWARGS by instroducing Dictionaries
+'''
+# *args! (**kwargs)
 # data structure -- called a dictionary
 
-# Example Dictonary English to German
-# Good day - Guten Tag
+# Explaination of 'Dictionary':
+# Example: Dictonary English to German
+#                    Good day - Guten Tag
 
 greetings = {
     "key": "value",
-    "table": "a place to poot your food on",
+    "table": "a place to put your food on",
     "car": "a maschine to help humans to move from point A to point B"
 }
 
-def full_name(*food):
-    full_string = ""
+#def hello(first_name, last_name="Doe")
+
+'''
+NOTE:
+def FUNCTION(ARGS 1st!!!, KWARGS 2nd!!!)
+'''
+
+def full_name(*args, **kwargs):        # kwargs are DICTIONARIES
+    print("--kwargs: ->", kwargs)
+    print("---args: ->", args)
+    first_name = kwargs["first_name"]
+    last_name = kwargs["last_name"]
+    return f"{args[0]} {first_name} {last_name}"
+
+# how to use it :
+
+print(full_name("Good morning", first_name = "Sharlene", last_name = "Doe", location = "Berlin"))
+
+# how to get data from people:
+print(full_name(first_name=input("Hello", "What is your first name? ", last_name="Doe")))
 
