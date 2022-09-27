f1d78b9d2573400136a819a2b276b7e103c402ce
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-27 09:17:58 +0200

    Add Kwargs.py to Funtions folder

---
+++ Exercises/Functions/Functions_Exercises/Kwargs.py
@@ -0,0 +1,20 @@
+############################################
+####### KWARGS (Keywords arguments) ########
+############################################
+
+
+# *args! (*kwargs)
+# data structure -- called a dictionary
+
+# Example Dictonary English to German
+# Good day - Guten Tag
+
+greetings = {
+    "key": "value",
+    "table": "a place to poot your food on",
+    "car": "a maschine to help humans to move from point A to point B"
+}
+
+def full_name(*food):
+    full_string = ""
+
