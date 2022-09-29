ea90792b49c11a466ee4aa44720a6cec0238b3ef
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-29 09:22:27 +0200

    Add DataType Folder and DataType.py

---
+++ Exercises/DataTypes/Data_Types.py
@@ -0,0 +1,25 @@
+'''
+As of today we know primitive data types have we seen?
+
+- strings
+- integer
+- float
+- boolean
+
+----------
+Compound data type
+---------
+- Lists
+- Tuples
+
+
+'''
+
+# What is a tuple?
+
+# it is an ordered sequence of elements, can mix element types of data. e.g.:
+
+name = ("Miri", "Lucas")
+other_name = ("Peer", "Hoffman")
+
+print(name)
